02920ea9ab55048ee34bc2fcb6ec6944ad3fe7cc
Jameson Trinker <jameson.trinker@gmail.com>
2013-10-22 16:09:01 -0500

    add ability to create tickets within a project

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,9 @@
 class TicketsController < ApplicationController
 	# bc tickets are a nested resourcse we have to set the project that the ticket belongs to
 	before_action :set_project
+	# :set_ticket finds the associated ticket within the scope of the project so we
+	# we don't have to do @ticket = @project.tickets... for all the actions
+	before_action :set_ticket, only: [:show, :edit, :update, :destroy]
 
   def index
   end
@@ -10,6 +13,14 @@ class TicketsController < ApplicationController
   end
 
   def create
+  	@ticket = @project.tickets.build(ticket_params)
+  	if @ticket.save
+  		flash[:notice] = "Ticket has been created."
+  		redirect_to project_ticket_path(@project, @ticket)
+  	else
+  		flash[:alert] = "Ticket was not created."
+  		render "new"
+  	end
   end
 
   def show
@@ -17,9 +28,17 @@ class TicketsController < ApplicationController
 
   private
 
+  	def ticket_params
+  		params.require(:ticket).permit(:title, :description)
+  	end
+
   	def set_project
   		# we're using params[:project_id] bc since we are in the TicketsController, params[:id] is
   		# referring to the id of the ticket.
   		@project = Project.find(params[:project_id])
+  	end
+
+  	def set_ticket
+  		@ticket = @project.tickets.find(params[:id])
   	end
 end
